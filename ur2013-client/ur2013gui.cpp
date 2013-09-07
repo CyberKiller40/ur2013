c98f872b99c51cdff155d60305a932f1fb552001
Łukasz "Cyber Killer" Korpalski <cyberkiller8@gmail.com>
2013-09-07 23:21:24 +0200

    base64! :-D
    --HG--
    extra : convert_revision : cyberkiller8%40gmail.com-20130907212124-1zaf0qd44q7h84vm

--- ur2013-client/ur2013gui.cpp
+++ ur2013-client/ur2013gui.cpp
@@ -18,6 +18,7 @@
 
 #include "ur2013gui.h"
 #include "ui_ur2013gui.h"
+#include <QDebug>
 
 ur2013gui::ur2013gui(QWidget *parent) :
 	QWidget(parent),
@@ -31,7 +32,13 @@ ur2013gui::ur2013gui(QWidget *parent) :
 	ui->hints->removeTab(3);
 	ui->hints->removeTab(2);
 	ui->hints->removeTab(1);
-
+	ui->label->setText(trUtf8(QByteArray::fromBase64("SmVzdCA4IGhpbnTDs3cgaSBuaWUga2HFvGR5IHogbmljaCBqZXN0IHJlYWxuxIUgcG9kcG93aWVkemnEhSA6LSkuPGJyIC8+PGJyIC8+QsSZZMSFIHNpxJkgb2RzxYJhbmlhxIcgeiBjemFzZW0gKG5hIMW8eXdvIHBvZGN6YXMgZHppYcWCYW5pYSBwcm9ncmFtaWt1KS4=")));
+	ui->label_2->setText(trUtf8(QByteArray::fromBase64("PGh0bWw+PGhlYWQvPjxib2R5PjxwPkl0J3MgZGFuZ2Vyb3VzIHRvIGdvIGFsb25lLCB0YWtlIDxhIGhyZWY9Imh0dHA6Ly96eGluZy5vcmcvdy9kZWNvZGUuanNweCI+PHNwYW4gc3R5bGU9IiB0ZXh0LWRlY29yYXRpb246IHVuZGVybGluZTsgY29sb3I6IzAwNTdhZTsiPnRoaXM8L3NwYW4+PC9hPi48L3A+PHA+KEx1YiBtaWVqIHBvZCByxJlrxIUgY3p5dG5payBRUkNvZGUgbmEga29tw7NyY2UpPC9wPjwvYm9keT48L2h0bWw+")));
+	ui->label_3->setText(trUtf8(QByteArray::fromBase64("THVkemllIG5pZ2R5IG5pZSBjenl0YWrEhSByYW5kb20gY29tbWVudMOzdyA6LVAuPGJyIC8+PGJyIC8+QSBwcnplc3Rhd2lhbmllIHplZ2Fya2EgdyBzeXN0ZW1pZSBuaWUgcG9tYWdhLg==")));
+	ui->label_4->setText(trUtf8(QByteArray::fromBase64("PGh0bWw+PGhlYWQvPjxib2R5PjxwPlRlcmF6IGJ5IHNpxJkgcHJ6eWRhxYJvIHRvIG1pZcSHIGNvIG5pZT88L3A+PHA+PGltZyBzcmM9IjovaW1nL3Jlcy90LnBuZyIvPjwvcD48L2JvZHk+PC9odG1sPg==")));
+	ui->label_5->setText(trUtf8(QByteArray::fromBase64("VG8gcG93ecW8ZWogdG8gbmllIGplc3Qgd3Byb3N0IGJhc2U2NC48YnIgLz48YnIgLz4uLi5pIHBvdHJ6ZWJueSBqZXN0IGRvc3TEmXAgZG8gc2llY2ksIGJleiB0ZWdvIGxpY3puaWsgbmllIGR6aWHFgmEgcG9wcmF3bmllLg==")));
+	ui->label_6->setText(trUtf8(QByteArray::fromBase64("PGh0bWw+PGhlYWQvPjxib2R5PjxwPk1vxbxlIHRlZ28gc3p1a2Fzej88L3A+PHA+PGltZyBzcmM9IjovaW1nL3Jlcy9jLnBuZyIvPjwvcD48L2JvZHk+PC9odG1sPg==")));
+	ui->label_7->setText(trUtf8(QByteArray::fromBase64("PGh0bWw+PGhlYWQvPjxib2R5PjxwPk9LLCBiZXogxZtjaWVteSwgdHUgamVzdCBvZHBvd2llZMW6OjwvcD48cD48aW1nIHNyYz0iOi9pbWcvcmVzL3IucG5nIi8+PC9wPjwvYm9keT48L2h0bWw+")));
 	//czas z netu
 	QNetworkAccessManager *manager = new QNetworkAccessManager(this);
 	connect(manager, SIGNAL(finished(QNetworkReply*)),this, SLOT(replyFinished(QNetworkReply*)));
@@ -67,7 +74,8 @@ void ur2013gui::setDisplay()
 	text=text.toUtf8().toBase64();
 	std::reverse(text.begin(),text.end());
 	if(InSeconds>0) ui->timerlabel->setText(text);
-	else ui->timerlabel->setText(trUtf8("Ciasto jest!\n(lub już było)"));
+	else ui->timerlabel->setText(trUtf8(QByteArray::fromBase64("Q2lhc3RvIGplc3QhPGJyIC8+KGx1YiBqdcW8IGJ5xYJvKQ==")));
+
 	if(InSeconds<5400 && hint2show==false)
 	{
 		ui->hints->addTab(ui->tab_2,"Hint 2");
